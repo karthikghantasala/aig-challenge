9e96511526f29ee776a3ce064fdb1c66e3101dd9
Karthik Ghantasala <karthikghan@gmail.com>
2017-09-01 01:28:33 -0400

    working!!!

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+
+#default['aig_project']['loc_choice'] = 'loca'
+
+default['aig_project']['loc_choice'] = ''
+
+default['aig_project']['db_root_user'] = 'postgres'
+default['aig_project']['db_root_passwd'] = 'qaz123wsx'
+
